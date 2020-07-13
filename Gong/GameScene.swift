5b7bb313f4dcf1114cd7d3d2945a92f5b030639e
Tyson Mak <tysonjmak@gmail.com>
2020-07-12 18:35:04 -0700

    Added project files

--- Gong/GameScene.swift
+++ Gong/GameScene.swift
@@ -1,110 +1,81 @@
-//
-//  GameScene.swift
-//  Gong
-//
-//  Created by Tyson Mak on 7/12/20.
-//  Copyright © 2020 Tyson Mak. All rights reserved.
-//
-
 import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+	
+	// Constants
+	let DEFAULT_PADDLE_WIDTH: CGFloat = 80
+	let DEFAULT_PADDLE_HEIGHT: CGFloat = 16
+	
+	// Input management
+	var touching = false
+	var touchPosition: CGPoint = CGPoint(x: 0, y: 0)
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+	// Time management
+	private var lastUpdateTime : TimeInterval = 0
+	
+	// Sprites
+	var centerLine: SKSpriteNode!
+	var playerGoal: SKSpriteNode!
+	var enemyGoal: SKSpriteNode!
+	
+	var player: SKSpriteNode!
+	var enemy: SKSpriteNode!
+		
+	// INIT SCENE
     override func sceneDidLoad() {
-
         self.lastUpdateTime = 0
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
+		
+		// Set background color
+		backgroundColor = UIColor(red: 24/255, green: 20/255, blue: 37/255, alpha: 1)
+		
+		// Center line divider
+		centerLine = SKSpriteNode(color: UIColor(red: 90/255, green: 105/255, blue: 136/255, alpha: 1), size: CGSize(width: frame.size.width, height: 5))
+		centerLine.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
+		addChild(centerLine!)
+		
+		// Player & Enemy sore zones
+		playerGoal = SKSpriteNode(color: UIColor(red: 0, green: 153/255, blue: 219/255, alpha: 1), size: CGSize(width: frame.size.width, height: DEFAULT_PADDLE_HEIGHT))
+		playerGoal.position = CGPoint(x: frame.size.width / 2, y: playerGoal.size.height / 2)
+		addChild(playerGoal!)
+		
+		enemyGoal = SKSpriteNode(color: UIColor(red: 1, green: 0, blue: 68/255, alpha: 1), size: CGSize(width: frame.size.width, height: DEFAULT_PADDLE_HEIGHT))
+		enemyGoal.position = CGPoint(x: frame.size.width / 2 , y: frame.size.height - enemyGoal.size.height / 2)
+		addChild(enemyGoal!)
+		
+		// Player & Enemy
+		player = SKSpriteNode(color: .white, size: CGSize(width: DEFAULT_PADDLE_WIDTH, height: DEFAULT_PADDLE_HEIGHT))
+		player.position = CGPoint(x: (size.width / 2), y: 100)
+		addChild(player!)
+		
+		enemy = SKSpriteNode(color: .white, size: CGSize(width: DEFAULT_PADDLE_WIDTH, height: DEFAULT_PADDLE_HEIGHT))
+		enemy.position = CGPoint(x: (size.width / 2), y: frame.size.height - 100)
+		addChild(enemy!)
+	}
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
+	// UPDATE GAME
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
         if (self.lastUpdateTime == 0) {
             self.lastUpdateTime = currentTime
         }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
+		
+		// Set the delta time
+		let dt = currentTime - self.lastUpdateTime
         self.lastUpdateTime = currentTime
     }
+	
+	// INPUT MANAGEMENT
+	override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+		for touch in touches {
+			let location = touch.location(in: self)
+			
+			if location.x + (player.size.width / 2) >= frame.size.width {
+				player.position.x = frame.size.width - (player.size.width / 2)
+			} else if location.x - (player.size.width / 2) <= 0 {
+				player.position.x = player.size.width / 2
+			} else {
+				player.position.x = location.x
+			}
+		}
+	}
 }
